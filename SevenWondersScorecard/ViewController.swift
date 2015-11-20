df51b583286e1b5e66df121202ae8a4d33886083
Jason Gaare <jason.d.gaare@gmail.com>
2015-11-20 11:52:03 -0600

    Player column completed on Storyboard, and populated with names

--- SevenWondersScorecard/ViewController.swift
+++ SevenWondersScorecard/ViewController.swift
@@ -8,11 +8,38 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate {
 
+    // MARK: Properties
+    
+    // Player Name Column
+    @IBOutlet weak var playerNameLabel: UILabel!
+    @IBOutlet weak var p1Name: UITextField!
+    @IBOutlet weak var p2Name: UITextField!
+    @IBOutlet weak var p3Name: UITextField!
+    @IBOutlet weak var p4Name: UITextField!
+    @IBOutlet weak var p5Name: UITextField!
+    @IBOutlet weak var p6Name: UITextField!
+    @IBOutlet weak var p7Name: UITextField!
+
+    
+    
+    // MARK: Standard Fuctions
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        p1Name.delegate = self
+        p2Name.delegate = self
+        p3Name.delegate = self
+        p4Name.delegate = self
+        p5Name.delegate = self
+        p6Name.delegate = self
+        p7Name.delegate = self
+
+        populateNames()
+        
+        print("Successful Load")
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +47,27 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    
+    func populateNames() {
+        p1Name.text = "Jason"
+        p2Name.text = "Erin"
+        p3Name.text = "Evan"
+        p4Name.text = "Amber"
+        p5Name.text = "Nate"
+        p6Name.text = "Jackie"
+        p7Name.text = "Marshall"
+    }
 
+    // MARK: UITextDelegate
+    func textFieldShouldReturn(textField: UITextField) -> Bool {
+        // Hide the keyboard.
+        textField.resignFirstResponder()
+        return true
+    }
+    
+    
+    
+    
+    
 }
 
