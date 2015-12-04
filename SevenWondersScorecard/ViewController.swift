245b397ed19ef17937406733ef713f2d984b7da9
Jason Gaare <jason.d.gaare@gmail.com>
2015-12-04 00:16:28 -0600

    New colors, keyboard scroll works. CHECKPOINT

--- SevenWondersScorecard/ViewController.swift
+++ SevenWondersScorecard/ViewController.swift
@@ -20,11 +20,13 @@ class ViewController: UIViewController, UITextFieldDelegate {
 
         // Once the view loads, we know how wide our frames are for scoring
         scoresToAddWidth = Int(p1MilScore.frame.width)
+        scoreSwitch.transform = CGAffineTransformMakeScale(0.75, 0.75)
         
         // Set Delegates
         setTextFieldDelegates()
         
         // Initialize final visual elements
+        mainScrollView.layer.cornerRadius = 11
         setBackgroundColors()
         populatePlayerNames()
         refreshScores()
