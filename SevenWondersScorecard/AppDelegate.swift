54e8c9336f2b1a3bfba663a07d30c766b4a39ce6
Jason Gaare <jason.d.gaare@gmail.com>
2015-12-04 16:47:46 -0600

    Added launch screen, changed icon

--- SevenWondersScorecard/AppDelegate.swift
+++ SevenWondersScorecard/AppDelegate.swift
@@ -16,6 +16,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         // Override point for customization after application launch.
+        
+        //pause on that screen for a two seconds longer
+        sleep(2)
+        
         return true
     }
 
